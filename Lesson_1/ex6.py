694c72e75818b8d29a1d7637691b29fff2b45ffd
tomatos <aleksey.trifonov@gmail.com>
2021-04-13 09:50:26 +0300

    Шестое задание

---
+++ Lesson_1/ex6.py
@@ -0,0 +1,11 @@
+import sys
+
+grow_per_day = 1.1
+day = 1
+result = int(sys.argv[1])
+last_result = int(sys.argv[2])
+while result < last_result:
+    result *= grow_per_day
+    day += 1
+else:
+    print(day)
--- Lesson_1/main.py
+++ Lesson_1/main.py
@@ -3,3 +3,4 @@ import ex2
 import ex3
 import ex4
 import ex5
+import ex6
