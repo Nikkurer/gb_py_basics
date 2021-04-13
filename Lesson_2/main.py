45feb58ce477fa2ceceafae8ddabc1dcae39060e
tomatos <8029506+nikkurer@users.noreply.github.com>
2021-04-13 21:00:52 +0300

    Попрвка - пятое задание =)

--- Lesson_2/main.py
+++ Lesson_2/main.py
@@ -1,9 +1,9 @@
 import ex1
 import ex2
 import ex3
-import ex4
+import ex5
 
 ex1.elements_type()
 print(ex2.elements_exchange())
 print(ex3.detect_season())
-print(ex4.rating())
+print(ex5.rating())
