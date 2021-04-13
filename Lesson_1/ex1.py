f37b22be04e2a37acef42a2db40783f514f9ccfe
tomatos <aleksey.trifonov@gmail.com>
2021-04-13 07:54:08 +0300

    Поправил ошибку

--- Lesson_1/ex1.py
+++ Lesson_1/ex1.py
@@ -1,4 +1,4 @@
 message = 'Привет!'
 print(message)
 name, age = input("Введите свои имя и возраст:").split()
-print(f'{name.capitalize()} вам {age} лет')
+print(f'{name.capitalize()}, вам {age} лет')
