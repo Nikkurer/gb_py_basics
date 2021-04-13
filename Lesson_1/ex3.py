e81d938d3a24d3bdaa481f39f12722fbc754b957
tomatos <aleksey.trifonov@gmail.com>
2021-04-13 08:03:33 +0300

    Улучшил форматирование

--- Lesson_1/ex3.py
+++ Lesson_1/ex3.py
@@ -4,4 +4,5 @@ message = 'Введите число:'
 number = input(message)
 for cycle in range(cycles):
     numbers.append(int(number*(cycle+1)))
-print(sum(numbers))
+print('Сумма чисел {} равна {:,}'.format(numbers, sum(numbers)).replace(',',
+                                                                       ' '))
