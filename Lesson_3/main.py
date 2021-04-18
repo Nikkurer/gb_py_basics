6c3a44dc037c759fbe0b0afa49be63f3a40eed41
tomatos <8029506+Nikkurer@users.noreply.github.com>
2021-04-18 14:25:57 +0300

    Пятое задание

--- Lesson_3/main.py
+++ Lesson_3/main.py
@@ -1,7 +1,8 @@
 import ex1
+import ex2
 import ex3
 import ex4
-from ex2 import print_user_info
+import ex5
 
 numbers = (5, 2)
 print(f'Задание 1:\n{ex1.devision(*numbers)}\n')
@@ -10,12 +11,14 @@ print('Задание 2')
 user = {'имя': 'Alex', 'фамилия': 'Johnson', 'год рождения': 1976,
         'город проживания': 'Los Angeles', 'email': 'ajohnson@contoso.com',
         'телефон': '+1(213)893-1239'}
-print_user_info(name=user['имя'], surname=user['фамилия'],
-                birth_year=user['год рождения'],
-                city=user['город проживания'], email=user['email'],
-                phone_number=user['телефон'])
+ex2.print_user_info(name=user['имя'], surname=user['фамилия'],
+                    birth_year=user['год рождения'],
+                    city=user['город проживания'], email=user['email'],
+                    phone_number=user['телефон'])
 print('\n')
 
 print(f'Задание 3:\n{ex3.my_func(-3, -5, 2)}\n')
 
-print(f'Задание 3:\n{ex4.my_func(2, -5)}')
+print(f'Задание 3:\n{ex4.my_func(2, -5)}\n')
+
+print(ex5.sum_input(), '\n')
