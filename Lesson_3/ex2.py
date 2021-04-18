aebc750b101cfb0247821fa7bb1cb865d0d3d9e1
tomatos <8029506+nikkurer@users.noreply.github.com>
2021-04-18 11:51:05 +0300

    Второе задание

---
+++ Lesson_3/ex2.py
@@ -0,0 +1,35 @@
+# Реализовать функцию, принимающую несколько параметров, описывающих данные
+# пользователя: имя, фамилия, год рождения, город проживания, email, телефон.
+# Функция должна принимать параметры как именованные аргументы. Реализовать
+# вывод данных о пользователе одной строкой.
+
+def print_user_info(**fields):
+    output = ''
+    for field, value in fields.items():
+        output = f'{output}{field}: {value} '
+    print(output)
+
+
+def user_input(func):
+    try:
+        return func(input(prompt))
+    except ValueError:
+        print('Некорректный ввод! Введите корректные данные')
+
+
+if __name__ == '__main__':
+    user = {'имя': str, 'фамилия': str, 'год рождения': int,
+            'город проживания': str, 'email': str, 'телефон': str}
+
+    print('Введите данные о пользователе: ')
+    for field, value in user.items():
+        prompt = f'{field}: '
+        success = False
+        while not success:
+            user[field] = user_input(value)
+            success = True
+
+    print_user_info(name=user['имя'], surname=user['фамилия'],
+                    birth_year=user['год рождения'],
+                    city=user['город проживания'], email=user['email'],
+                    phone_number=user['телефон'])
--- Lesson_3/main.py
+++ Lesson_3/main.py
@@ -1,4 +1,13 @@
 import ex1
+from ex2 import print_user_info
 
 numbers = (5, 2)
 print(ex1.devision(*numbers))
+
+user = {'имя': 'Alex', 'фамилия': 'Johnson', 'год рождения': 1976,
+        'город проживания': 'Los Angeles', 'email': 'ajohnson@contoso.com',
+        'телефон': '+1(213)893-1239'}
+print_user_info(name=user['имя'], surname=user['фамилия'],
+                birth_year=user['год рождения'],
+                city=user['город проживания'], email=user['email'],
+                phone_number=user['телефон'])
