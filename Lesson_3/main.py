4e2558a794041c4e165da63f0b36809a5fdab26e
tomatos <8029506+nikkurer@users.noreply.github.com>
2021-04-18 12:16:55 +0300

    Третье задание

--- Lesson_3/main.py
+++ Lesson_3/main.py
@@ -1,9 +1,11 @@
 import ex1
 from ex2 import print_user_info
+from ex3 import my_func
 
 numbers = (5, 2)
-print(ex1.devision(*numbers))
+print(f'Задание 1:\n{ex1.devision(*numbers)}\n')
 
+print('Задание 2')
 user = {'имя': 'Alex', 'фамилия': 'Johnson', 'год рождения': 1976,
         'город проживания': 'Los Angeles', 'email': 'ajohnson@contoso.com',
         'телефон': '+1(213)893-1239'}
@@ -11,3 +13,6 @@ print_user_info(name=user['имя'], surname=user['фамилия'],
                 birth_year=user['год рождения'],
                 city=user['город проживания'], email=user['email'],
                 phone_number=user['телефон'])
+print('\n')
+
+print(f'Задание 3:\n{my_func(-3, -5, 2)}')
