d02c59b113f5c2938484f2306e555fd758d28eea
tomatos <8029506+Nikkurer@users.noreply.github.com>
2021-04-22 08:44:56 +0300

    Первое задание

---
+++ Lesson_4/ex1.py
@@ -0,0 +1,19 @@
+# Реализовать скрипт, в котором должна быть предусмотрена функция расчета
+# заработной платы сотрудника. В расчете необходимо использовать формулу:
+# (выработка в часах*ставка в час) + премия. Для выполнения расчета для
+# конкретных значений необходимо запускать скрипт с параметрами.
+
+import sys
+
+
+def salary(x, y, z):
+    return x * y + z
+
+
+if __name__ == '__main__':
+    hours_worked = float(sys.argv[1])
+    rate = float(sys.argv[2])
+    premium = float(sys.argv[3])
+    print(salary(hours_worked, rate, premium))
+
+    print(lambda )
---
+++ Lesson_4/main.py
@@ -0,0 +1,6 @@
+import ex1
+
+hours_worked = 800
+rate = 750
+premium = 60000
+ex1.salary(hours_worked, rate, premium)
