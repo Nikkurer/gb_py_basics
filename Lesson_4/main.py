054bff4cc6c28828cdde454b07d5ed6b3a1bd498
tomatos <8029506+Nikkurer@users.noreply.github.com>
2021-04-22 08:59:02 +0300

    Третье задание

--- Lesson_4/main.py
+++ Lesson_4/main.py
@@ -1,9 +1,12 @@
 import ex1
 import ex2
+import ex3
 
 hours_worked = 800
 rate = 750
 premium = 60000
-ex1.salary(hours_worked, rate, premium)
+print(ex1.salary(hours_worked, rate, premium))
 
-ex2.new_list(ex2.original_list)
+print(ex2.new_list(ex2.original_list))
+
+print(ex3.result)
