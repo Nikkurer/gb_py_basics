80182f28392031cb9281c0a15518400a693f64a7
tomatos <8029506+Nikkurer@users.noreply.github.com>
2021-05-04 09:44:32 +0300

    Третье задание

---
+++ Lesson_5/ex3.py
@@ -0,0 +1,26 @@
+# Создать текстовый файл (не программно), построчно записать фамилии
+# сотрудников и величину их окладов (не менее 10 строк). Определить, кто из
+# сотрудников имеет оклад менее 20 тыс., вывести фамилии этих сотрудников.
+# Выполнить подсчет средней величины дохода сотрудников.
+#
+# Пример файла:
+# Иванов 23543.12
+# Петров 13749.32
+import random
+
+a = 123
+result = ''
+salary_min = 20000
+salary_sum = 0
+
+with open('surnames.txt', mode='rt', encoding='utf-8') as file:
+    employees = tuple(file.readlines())
+    employee_number = random.randint(10, 20)
+    print('Сотрудники с окладом менее 20000:')
+    for employee_raw in random.sample(employees, employee_number):
+        employee, salary = employee_raw.split()
+        salary = int(salary)
+        salary_sum += salary
+        if salary < salary_min:
+            print(employee)
+    print(f'Средний доход сотрудника: {salary_sum / employee_number:.2f}')
