bb399f4ad5dfcfccf4d3e0940914c302ac1e25ea
tomatos <8029506+Nikkurer@users.noreply.github.com>
2021-05-04 11:33:38 +0300

    Третье задание. Улучшил читаемость

--- Lesson_5/ex3.py
+++ Lesson_5/ex3.py
@@ -13,9 +13,10 @@ result = ''
 salary_min = 20000
 salary_sum = 0
 
-with open('ex3.txt', mode='rt', encoding='utf-8') as file:
-    employees = tuple(file.readlines())
+with open('ex3.txt', mode='rt', encoding='utf-8') as input_file:
+    employees = tuple(input_file)
     employee_number = random.randint(10, 20)
+    # employee_number = len(employees)
     print('Сотрудники с окладом менее 20000:')
     for employee_raw in random.sample(employees, employee_number):
         employee, salary = employee_raw.split()
