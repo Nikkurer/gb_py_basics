d4af3220329b50d2dc786d578dcba4cb9586e7f9
tomatos <8029506+Nikkurer@users.noreply.github.com>
2021-05-04 15:46:43 +0300

    Седьмое задание. Убрал генерацию файла

--- Lesson_5/ex7.py
+++ Lesson_5/ex7.py
@@ -21,41 +21,20 @@
 # [{"firm_1": 5000, "firm_2": 3000, "firm_3": 1000}, {"average_profit": 2000}]
 # Подсказка: использовать менеджер контекста.
 import json
-import random
 
-
-def generate_file():
-    result = ''
-    right_forms = ('ООО', 'ИП', 'АО', 'ПАО')
-    company_names = ('Тудранктуфак', 'Валенок', 'Звёздочки', 'Спрос',
-                     'Скалолаз', 'Щегол', 'Свиристель', 'Строители', 'Аврора',
-                     'Ы')
-    with open('ex7.txt', 'w', encoding='utf-8') as output_file:
-        for company_name in company_names:
-            right_form = random.choice(right_forms)
-            proceeds = random.randint(0, 100000)
-            costs = random.randint(-100000, 100000)
-            company = f'{company_name} {right_form} {proceeds} {costs}'
-            result = f'{result} {company}\n'
-        output_file.write(result)
-
-
-if __name__ == '__main__':
-    generate_file()
-    with open('ex7.txt', 'r', encoding='utf-8') as input_file:
-        balance_sum = 0
-        companies_dict = {}
-        for line in input_file:
-            company_raw = line.split()
-            company_name = company_raw[0]
-            company_proceed = int(company_raw[2])
-            company_costs = int(company_raw[3])
-            balance = company_proceed - company_costs
-            if balance > 0:
-                balance_sum += balance
-            companies_dict[company_name] = balance
-    average_profit = {"average_profit": balance_sum / len(companies_dict)}
-    companies_list = [companies_dict, average_profit]
-    with open('ex7_out.txt', 'wt', encoding='utf-8') as output_file:
-        output_file.write(json.dumps(companies_list, ensure_ascii=False,
-                                     indent=4))
+with open('ex7.txt', 'r', encoding='utf-8') as input_file:
+    balance_sum = 0
+    companies_dict = {}
+    for line in input_file:
+        company_raw = line.split()
+        company_name = company_raw[0]
+        company_proceed = int(company_raw[2])
+        company_costs = int(company_raw[3])
+        balance = company_proceed - company_costs
+        if balance > 0:
+            balance_sum += balance
+        companies_dict[company_name] = balance
+average_profit = {"average_profit": balance_sum / len(companies_dict)}
+companies_list = [companies_dict, average_profit]
+with open('ex7_out.txt', 'wt', encoding='utf-8') as output_file:
+    output_file.write(json.dumps(companies_list, ensure_ascii=False, indent=4))
