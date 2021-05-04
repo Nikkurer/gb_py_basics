a23aca6cd4e5711afa0df094a8475972ce81511e
tomatos <8029506+Nikkurer@users.noreply.github.com>
2021-05-04 12:01:56 +0300

    Пятое задание

---
+++ Lesson_5/ex5.py
@@ -0,0 +1,16 @@
+# Создать (программно) текстовый файл, записать в него программно набор
+# чисел, разделенных пробелами. Программа должна подсчитывать сумму чисел в
+# файле и выводить ее на экран.
+
+import random
+
+numbers_list = [random.randrange(1, 50) for i in range(20)]
+result = ' '.join(map(str, numbers_list))
+
+with open('ex5.txt', 'wt', encoding='utf-8') as output_file:
+    output_file.write(result)
+
+with open('ex5.txt', 'rt', encoding='utf-8') as input_file:
+    numbers_raw = input_file.readline()
+    numbers_sum = sum(map(int, numbers_raw.split()))
+    print(numbers_sum)
