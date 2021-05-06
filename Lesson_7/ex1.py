5393040a4383f8ad55e608e0a10252970b3ee3b0
tomatos <8029506+Nikkurer@users.noreply.github.com>
2021-05-06 09:25:52 +0300

    Первое задание. Реализация сложения

--- Lesson_7/ex1.py
+++ Lesson_7/ex1.py
@@ -27,21 +27,40 @@ class Matrix:
         self.data = []
         self.rows = rows
         self.columns = columns
+
+    def fill_matrix(self):
         for row in range(self.rows):
             self.data.append(
-                [number for number in random.sample(range(20), self.columns)])
+                [number for number in random.sample(range(99), self.columns)])
+
+    def __len__(self):
+        return len(self.data)
 
     def __str__(self):
         matrix_str = ''
         for row in self.data:
+            # TODO: Заменить вложенный цикл на функцию
             for element in row:
                 matrix_str = f'{matrix_str}{element:2d} '
             matrix_str = f'{matrix_str}\n'
         return matrix_str
 
+    def add(self, matrix):
+        result = Matrix(self.rows, self.columns)
+        for row in range(self.rows):
+            result.data.append([])
+            for element in range(self.columns):
+                new_element = self.data[row][element] + matrix.data[row][
+                    element]
+                result.data[row].append(new_element)
+        return result
+
 
 if __name__ == '__main__':
     mx1 = Matrix(3, 3)
+    mx1.fill_matrix()
     mx2 = Matrix(3, 3)
-    print(mx1, '\n')
+    mx2.fill_matrix()
+    print(mx1)
     print(mx2)
+    print(mx1.add(mx2))
